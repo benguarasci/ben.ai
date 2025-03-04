7a6413e349c5e541f21335e9ba3dcb637a20ee57
benguarasci <55171799+benguarasci@users.noreply.github.com>
2025-03-04 00:48:18 -0800

    Update project

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  images: {
+    domains: ['rentsimple-images-west.s3.us-west-2.amazonaws.com'],
+  },
+}
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,8 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: [
+    "@tailwindcss/postcss",
+    // Add any other plugins here
+  ],
 };
 
 export default config;
